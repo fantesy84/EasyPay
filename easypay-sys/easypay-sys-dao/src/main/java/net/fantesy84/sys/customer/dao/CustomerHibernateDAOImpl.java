e5fb679126e731be30c113fd3d118cd62d8e5b88
Andronicus Cook <fantesy84@gmail.com>
2016-04-14 11:31:57 +0800

    修正个性化配置
    Signed-off-by: Andronicus Cook <fantesy84@gmail.com>
    家中则使用局域网中的服务器,在公司则使用VPS服务器

--- easypay-sys/easypay-sys-dao/src/main/java/net/fantesy84/sys/customer/dao/CustomerHibernateDAOImpl.java
+++ easypay-sys/easypay-sys-dao/src/main/java/net/fantesy84/sys/customer/dao/CustomerHibernateDAOImpl.java
@@ -2,22 +2,18 @@ package net.fantesy84.sys.customer.dao;
 
 import java.util.List;
 
-import org.hibernate.SessionFactory;
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
 import net.fantesy84.exception.EasypayException;
 import net.fantesy84.sys.customer.domain.EpSysCust;
 @Repository("customerHibernateDao")
-public class CustomerHibernateDAOImpl extends HibernateDaoSupport implements CustomerDAO {
+public class CustomerHibernateDAOImpl implements CustomerDAO {
 	@Autowired
-	public void setReadSessionFactory(@Qualifier("read01_SessionFactory")SessionFactory sessionFactory) {
-		super.setSessionFactory(sessionFactory);
-	}
+	private HibernateTemplate readHibernateTemplate;
 	
 	@Override
 	public int insert(EpSysCust cust) throws EasypayException {
@@ -30,7 +26,7 @@ public class CustomerHibernateDAOImpl extends HibernateDaoSupport implements Cus
 	public List<EpSysCust> selectByName(EpSysCust cust) throws EasypayException {
 		DetachedCriteria dc = DetachedCriteria.forClass(EpSysCust.class, "cust");
 		dc.add(Restrictions.eq("cust.custName", cust.getCustName()));
-		return (List<EpSysCust>) super.getHibernateTemplate().findByCriteria(dc);
+		return (List<EpSysCust>) readHibernateTemplate.findByCriteria(dc);
 	}
 
 }
