f4b7f9128083e66fd309f81a0279cbeb0d75111a
Andronicus Cook <fantesy84@gmail.com>
2016-05-05 17:55:13 +0800

    完成任务监听和远程任务调用示例

---
+++ easypay-schedule/easypay-schedule-web/src/test/java/net/fantesy84/test/quartz/example/rmi/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Project: easypay-schedule-web
+ * Created: 2016年5月5日
+ * ©gopay.com Inc.
+ */
+/**
+ * Description:
+ * <P>
+ * @author junjie.ge
+ * @since JDK1.7
+ */
+package net.fantesy84.test.quartz.example.rmi;
